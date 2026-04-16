a4dade342d30e944d8db8fd104140ed54476d197
7xr <55927013+7xrrr@users.noreply.github.com>
2026-04-16 22:42:05 +0100

    Create ViscaSub.js

---
+++ ViscaSub.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Visca Tools Loader
+// @namespace    http://tampermonkey.net/
+// @version      2.0.6
+// @description  Loads Visca Tools via jsDelivr CDN
+// @author       7xr
+// @match        *://discord.com/*
+// @run-at       document-idle
+// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/7xrrr/discord_tools@main/ViscaTools.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Visca Tools (CDN) loaded successfully!');
+})();
